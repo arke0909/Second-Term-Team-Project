2183f8784aab954ff21d8bc59a79de9ea32755d8
PSB08 <Parkseongbin08@gmail.com>
2024-09-23 09:17:00 +0900

    저장로드 만들기
    아직 테스트임

--- Assets/00.Work/PSB/01.Scripts/SaveLoad/GameData.cs
+++ Assets/00.Work/PSB/01.Scripts/SaveLoad/GameData.cs
@@ -2,16 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.iOS;
 
 [Serializable]
 public class GameData
 {
-    //instanc«“ øπ¡§
-
     public GameData(string _name, Vector3 _pos)
     {
-        go.name = _name;
+        go.name = name;
+        go.transform.position = pos;
+
+        name = _name;
         pos = _pos;
     }
     GameObject go = GameObject.Find("Test");
--- Assets/00.Work/PSB/01.Scripts/SaveLoad/TestBlock.cs
+++ Assets/00.Work/PSB/01.Scripts/SaveLoad/TestBlock.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class TestBlock : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))  //Save
         {
-            GameData character = new GameData("D", new Vector3(0 ,0 ,1));
+            GameData character = new GameData(gameObject.name, gameObject.transform.position);
+            
             DataController.Save(character, "save_001");
         }
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L))  //Load
         {
             GameData loadData = DataController.Load("save_001");
             Debug.Log(string.Format("LoadData Result => name : {0}, pos : {1}", loadData.name, loadData.pos));
+            gameObject.transform.position = loadData.pos;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(5f, rb.velocity.y);
     }
 }
