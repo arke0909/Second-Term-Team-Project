e729cca5b44f3cb025a0e9728206020dda747526
PSB08 <Parkseongbin08@gmail.com>
2024-09-26 16:26:47 +0900

    PushBlockScript Semi Complete
    기믹 조금 완성 Effector 활용해서 기믹 추가 중

--- Assets/00.Work/PSB/01.Scripts/Gimmick/PushBlockScript.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/PushBlockScript.cs
@@ -1,8 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PushBlockScript : MonoBehaviour
 {
+    public float pullForce = 5f;
+    public float detectionRange = 5f;
+    private Transform player; 
+    private Rigidbody2D rb;
 
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distance = Vector2.Distance(transform.position, player.position);
+
+            if (distance < detectionRange)
+            {
+                Vector2 direction = new Vector2(player.position.x - transform.position.x, 0).normalized;
+                rb.AddForce(direction * pullForce);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f, 0.1f); // R, G, B, A
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
 }
