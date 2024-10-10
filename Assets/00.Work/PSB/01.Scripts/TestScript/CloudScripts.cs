3794588ebf0f7208d841a3ae396b86e270623637
PSB08 <Parkseongbin08@gmail.com>
2024-10-11 00:45:34 +0900

    Add cloud gimmick, devide map, tilemap,

---
+++ Assets/00.Work/PSB/01.Scripts/TestScript/CloudScripts.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScripts : MonoBehaviour
+{
+    public float power = 5f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Cloud")
+        {
+            rb.velocity = new Vector2(rb.velocity.x, power);
+        }
+    }
+
+}
---
+++ Assets/00.Work/PSB/01.Scripts/TestScript/NextMap.cs
@@ -0,0 +1,29 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextMap : MonoBehaviour
+{
+    [SerializeField] private CapsuleCollider2D _playerCollider;
+
+
+    private void Awake()
+    {
+        _playerCollider = GetComponentInChildren<CapsuleCollider2D>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "NextMapBlock")
+        {
+            Debug.Log(1);
+            _playerCollider.transform.parent.position = new Vector3(_playerCollider.transform.position.x, _playerCollider.transform.position.y + 5,
+                _playerCollider.transform.position.z);
+            
+        }
+    }
+
+
+}
