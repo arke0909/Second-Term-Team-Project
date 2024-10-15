f175b924bf6fe5e310b985111df31570d6aabe97
PSB08 <Parkseongbin08@gmail.com>
2024-10-16 08:30:27 +0900

    All Destroy my map
    내 씬 내용 전부 삭제

--- Assets/00.Work/PSB/01.Scripts/Gimmick/CloudScripts.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/CloudScripts.cs
@@ -12,12 +12,13 @@ public class CloudScripts : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.tag == "Cloud")
+        if (collision.gameObject.tag == "Cloud")
         {
             rb.velocity = new Vector2(rb.velocity.x, power);
-            Debug.Log("yy");
         }
     }
 
