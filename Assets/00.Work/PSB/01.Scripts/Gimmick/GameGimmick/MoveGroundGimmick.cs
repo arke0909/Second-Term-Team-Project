16c648989f01e18213e0777a7a662b9be8718520
PSB08 <Parkseongbin08@gmail.com>
2024-11-02 22:39:23 +0900

    Complete MoveGround
    점프하거나 근처에 가면 한 번 멀어지는 블럭

--- Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/MoveGroundGimmick.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/MoveGroundGimmick.cs
@@ -1,18 +1,36 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class MoveGroundGimmick : MonoBehaviour
+public class MoveGroundGimmick : Gimmick, IInitializable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject gimmick;
+    [SerializeField] private BoxCollider2D box2D;
+    private bool isRun = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.layer == 8 && isRun == false)
+        {
+            gimmick.transform.DOMoveX(gimmick.transform.position.x + 4, 0.1f).SetEase(Ease.Linear);
+            isRun = true;
+            box2D.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool Check()
     {
-        
+        return isRun;
     }
+
+    public void Initialize()
+    {
+        box2D = GetComponent<BoxCollider2D>();
+        gimmick = GetComponentInParent<GameObject>();
+    }
+
+    
+
 }
--- Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/TrainGimmick.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/TrainGimmick.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class TrainGimmick : MonoBehaviour
+public class TrainGimmick : Gimmick, IInitializable
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player")]
+    [SerializeField] private GameObject player;
+
+    [Header("Value")]
+    public float distanceValue = 10f;
+    public float arrowSpeed = 5f;
+    private Vector2 direction;
+
+    private bool isMove = false;
+
+    private void Update()
     {
-        
+        float distancePlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distancePlayer < distanceValue && !isMove)
+        {
+            StartMove();
+        }
+        else if (distancePlayer > distanceValue)
+        {
+            isMove = false;
+        }
+
+        if (isMove == true)
+        {
+            TrainMove();
+        }
+
+    }
+    public override bool Check()
+    {
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EffectGimmick()
     {
-        
+        gameObject.transform.position = new Vector3(0, 0, 0);
+
+        base.EffectGimmick();
     }
+
+    public void Initialize()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void StartMove()
+    {
+        Vector2 direct = (transform.position - player.transform.position).normalized;
+        transform.right = direct;
+
+        direction = (player.transform.position - transform.position).normalized;
+        isMove = true;
+    }
+
+    private void TrainMove()
+    {
+        transform.position += (Vector3)direction * arrowSpeed * Time.deltaTime;
+    }
+
 }
