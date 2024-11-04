955bdbdf8d269df38b8519d792fecba1387ebcf4
PSB08 <Parkseongbin08@gmail.com>
2024-11-05 01:48:27 +0900

    Enemy script and Sprite download
    에너미 만드는 중 및 에셋 다운

--- Assets/00.Work/PSB/01.Scripts/Gimmick/EnemyScript/EnemyMove.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/EnemyScript/EnemyMove.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     private Rigidbody2D rigid;
+    private Animator anim;
+    private SpriteRenderer sprite;
     public int nextMove;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        Invoke("Think", 5);
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        Invoke("Think", 3);
     }
 
     private void FixedUpdate()
@@ -18,21 +22,37 @@ public class EnemyMove : MonoBehaviour
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
         //플랫포머 체크
-        Vector2 frontVect = new Vector2(rigid.position.x + nextMove, rigid.position.y);
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
+        Vector2 frontVect = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        Debug.DrawRay(frontVect, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVect, Vector3.down, 1, LayerMask.GetMask("Ground"));
         if (rayHit.collider == null)
         {
-            nextMove *= -1;
-            CancelInvoke();
-            Invoke("Think", 5);
+            TurnEnemy();
         }
 
+    }
+
+    private void TurnEnemy()
+    {
+        nextMove *= -1;
+        sprite.flipX = nextMove == -1;
+        CancelInvoke();
+        Invoke("Think", 3);
     }
 
     private void Think()
     {
         nextMove = Random.Range(-1, 2);
-        Invoke("Think", 5);
+
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+        {
+            sprite.flipX = nextMove == -1;
+        }
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
     }
 
 }
