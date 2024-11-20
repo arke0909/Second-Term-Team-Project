29ed0b3c6de97ac17689ba6843464579e851e92d
arke0909 <arke0909@gmail.com>
2024-11-20 13:50:48 +0900

    사운드 플레이어 & 레벨 디자인

--- Assets/00.Work/EJY/01.Scripts/Feedbacks/Feedback.cs
+++ Assets/00.Work/EJY/01.Scripts/Feedbacks/Feedback.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public abstract class Feedback : MonoBehaviour
 {
-    public abstract void PlayFeedbak();
+    public abstract void PlayFeedback();
     public abstract void StopFeedback();
     private void OnDisable()
     {
--- Assets/00.Work/EJY/01.Scripts/Gimmick/Gimmick.cs
+++ Assets/00.Work/EJY/01.Scripts/Gimmick/Gimmick.cs
@@ -17,6 +17,7 @@ public abstract class Gimmick : MonoBehaviour
         try
         {
             used = true;
+            Debug.Log(1);
         }
         catch(Exception ex)
         {
