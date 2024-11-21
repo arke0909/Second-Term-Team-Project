22ce3e7ce1a00930f18601944cbca5445fcf719a
PSB08 <Parkseongbin08@gmail.com>
2024-11-21 17:05:16 +0900

    Merge

--- Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/ShowBlockEffector.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/ShowBlockEffector.cs
@@ -2,73 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowBlockEffector : Gimmick
+public class ShowBlockEffector : MonoBehaviour
 {
-    [SerializeField] private PlatformEffector2D effector;
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    private PlatformEffector2D effector;
+    private SpriteRenderer spriteRenderer;
 
-    private bool isVisible = false;
-
-    public float distance = 10f;
-
-    private void Start()
-    {
-        effector.enabled = true;
-        spriteRenderer.enabled = false;
-    }
+    private bool used = false;
 
     private void Awake()
     {
-        isVisible = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        effector = GetComponent<PlatformEffector2D>();
+
+        spriteRenderer.enabled = false;
+        effector.enabled = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!isVisible)
+        if (!used)
         {
             effector.enabled = false;
             spriteRenderer.enabled = true;
+            used = true;
         }
     }
-
-    public override bool Check()
-    {
-        return true;
-    }
-
-    public override void EffectGimmick()
-    {
-    }
-
-    /*private void CheckArc()
-    {
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
-
-        if (hit.collider != null)
-        {
-            int layerMask = effector.colliderMask;
-
-            int playerLayer = LayerMask.NameToLayer("Player");
-
-            if (hit.collider.gameObject.layer == playerLayer)
-            {
-                float surfaceArc = effector.surfaceArc;
-                float angle = Vector2.Angle(Vector2.up, hit.normal);
-
-                if (angle <= surfaceArc / 2)
-                {
-                    spriteRenderer.enabled = true;
-                    effector.enabled = false;
-                }
-            }
-        }
-
-
-
-    }*/
-
-
-
 }
