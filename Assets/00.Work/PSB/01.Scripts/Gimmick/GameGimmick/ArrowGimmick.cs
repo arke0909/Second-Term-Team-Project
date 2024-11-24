503dc0e77693d5bb6fe02c64318d966c6118a061
PSB08 <Parkseongbin08@gmail.com>
2024-11-24 16:28:21 +0900

    LevelDesign Scene

--- Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/ArrowGimmick.cs
+++ Assets/00.Work/PSB/01.Scripts/Gimmick/GameGimmick/ArrowGimmick.cs
@@ -63,15 +63,6 @@ public class ArrowGimmick : Gimmick, IIntializable
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)  //Player Layer
-        {
-            player.SetActive(false);
-        }
-    }
-
-
     private void StartMove()
     {
         Vector2 direct = (player.transform.position - transform.position).normalized;
