4b3ba152aa7d878098f17598e1141d89f4f5c0f9
PSB08 <Parkseongbin08@gmail.com>
2024-11-25 14:35:26 +0900

    UI sound and BGM prefab

--- Assets/00.Work/PSB/01.Scripts/UI/ButtonEffect.cs
+++ Assets/00.Work/PSB/01.Scripts/UI/ButtonEffect.cs
@@ -6,8 +6,15 @@ using UnityEngine.EventSystems;
 
 public class ButtonEffect : MonoBehaviour, IPointerDownHandler
 {
+    [SerializeField] private AudioSource btnSound;
+    private void Start()
+    {
+        btnSound.Stop();
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        btnSound.Play();
         transform.DORewind();
         transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f), 0.25f).SetAutoKill(false).SetUpdate(true);
     }
